a473dba3e1a1a87424b2cc0f5f5e8c52827ff5a7
noemi <noemi.muste@thoughtworks.com>
2018-09-12 12:50:14 +0200

    Draw a horizontal line

--- src/main/java/com/thoughtworks/exercices/Main.java
+++ src/main/java/com/thoughtworks/exercices/Main.java
@@ -1,14 +1,36 @@
 package com.thoughtworks.exercices;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        //Triangle Exercises
         triangleExercises();
+
+        //Draw a horizontal line
+        drawHorizontalLine();
     }
 
     private static void triangleExercises(){
         System.out.println("Triangle Exercises");
         System.out.println("*");
+        System.out.println();
+    }
+
+    private static void drawHorizontalLine() {
+        System.out.println("Triangle Exercises");
+
+        //ask number
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce a number: ");
+        int n = scanner.nextInt();
+
+        //iterate n times to print n *
+        for(int i = 0; i < n; i++){
+            System.out.print("*");
+        }
+
         System.out.println();
     }
 }
