e1542814d4aa7dcc7f87a6abff03428d3f80cbd1
noemi <noemi.muste@thoughtworks.com>
2018-09-12 13:11:21 +0200

    Draw a right triangle + code refactoring

--- src/main/java/com/thoughtworks/exercices/Main.java
+++ src/main/java/com/thoughtworks/exercices/Main.java
@@ -6,50 +6,57 @@ public class Main {
 
     public static void main(String[] args){
         //Triangle Exercises
+        System.out.println("Triangle Exercises");
         triangleExercises();
+        System.out.println();
 
         //Draw a horizontal line
-        drawHorizontalLine();
+        System.out.println("Draw a horizontal line");
+        drawHorizontalLine(askNumber());
+        System.out.println();
 
         //Draw a vertical line
-        drawVerticalLine();
-    }
+        System.out.println("Draw a vertical line");
+        drawVerticalLine(askNumber());
+        System.out.println();
 
-    private static void triangleExercises(){
-        System.out.println("Triangle Exercises");
-        System.out.println("*");
+        //Draw a right triangle
+        System.out.println("Draw a right triangle");
+        drawARightTriangle(askNumber());
         System.out.println();
     }
 
-    private static void drawHorizontalLine() {
-        System.out.println("Draw a horizontal line");
 
-        //ask number
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduce a number: ");
-        int n = scanner.nextInt();
+    private static void triangleExercises(){
+        System.out.println("*");
+    }
 
+    private static void drawHorizontalLine(int n) {
         //iterate n times to print n *
         for(int i = 0; i < n; i++){
             System.out.print("*");
         }
 
-        System.out.println("\n");
+        System.out.println();
     }
 
-    private static void drawVerticalLine() {
-        System.out.println("Draw a vertical line");
-
-        //ask number
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduce a number: ");
-        int n = scanner.nextInt();
-
+    private static void drawVerticalLine(int n) {
         //iterate n times to print n *
         for(int i = 0; i < n; i++){
             System.out.println("*");
         }
+    }
 
-        System.out.println();
+    private static void drawARightTriangle(int n) {
+        //iterate n times to print n *
+        for(int i = 1; i <= n; i++){
+            drawHorizontalLine(i);
+        }
+    }
+
+    private static int askNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce a number: ");
+        return scanner.nextInt();
     }
 }
