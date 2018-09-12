daa280a01699c02100931df6d763064b09eeddcd
noemi <noemi.muste@thoughtworks.com>
2018-09-12 17:40:58 +0200

    Javadoc generated

--- src/main/java/com/thoughtworks/exercices/Main.java
+++ src/main/java/com/thoughtworks/exercices/Main.java
@@ -8,73 +8,73 @@ public class Main {
 
     public static void main(String[] args){
        System.out.println("TRIANGLE EXERCISES");
-        //Triangle Exercises
         System.out.println("Easiest exercise ever");
         triangleExercises();
         System.out.println();
 
-        //Draw a horizontal line
         System.out.println("Draw a horizontal line");
         drawHorizontalLine(askNumber());
         System.out.println();
 
-        //Draw a vertical line
         System.out.println("Draw a vertical line");
         drawVerticalLine(askNumber());
         System.out.println();
 
-        //Draw a right triangle
         System.out.println("Draw a right triangle");
         drawARightTriangle(askNumber());
         System.out.println();
 
         System.out.println("DIAMOND EXERCISES");
-        //Isosceles Triangle
         System.out.println("Isosceles Triangle");
         isoscelesTriangle(askNumber());
         System.out.println();
 
-        //Diamond
         System.out.println("Diamond");
         diamond(askNumber());
         System.out.println();
 
-        //Diamond with name
         System.out.println("Diamond with Name");
         diamondWithName(askNumber(), askName());
         System.out.println();
 
-        //FizzBuzz Exercise
-        System.out.println("FizzBuzz Exercise");
+        System.out.println("FizzBuzz EXERCISE");
         fizzBuzz();
         System.out.println();
 
-        //Prime Factors Exercise
-        System.out.println("Prime Factors Exercise");
+        System.out.println("PRIME FACTORS EXERCISE");
         primeFactorsExercise();
     }
 
-    private static void primeFactorsExercise() {
-        List<Integer> primeFactors = new ArrayList<>();
-        generate(askNumber(), primeFactors);
-
-        for(Integer fact : primeFactors){
-            System.out.println(fact);
-        }
-    }
-
+    /**
+     * Asks a number
+     * @return int --> number requested
+     */
     private static int askNumber(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce a number: ");
+
+        while (!scanner.hasNextInt()) {
+            String input = scanner.next();
+            System.out.printf("\"%s\" is not a valid number. Enter a new one: \n", input);
+        }
+
         return scanner.nextInt();
     }
 
+    /**
+     * Asks a name
+     * @return String --> name requested
+     */
     private static String askName(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce a name: ");
         return scanner.next();
     }
 
+    /**
+     * Given a number n, print n lines, each with one more asterisk than the last (i.e. one on the first line, two on the second,etc.)
+     * @param n
+     */
     private static void drawARightTriangle(int n) {
         //iterate n times to print n lines, each with one more asterisk than the last
         for(int i = 1; i <= n; i++){
@@ -82,10 +82,17 @@ public class Main {
         }
     }
 
+    /**
+     * Print one asterisk to the console.
+     */
     private static void triangleExercises(){
         System.out.println("*");
     }
 
+    /**
+     * Given a number n, print n asterisks on one line.
+     * @param n
+     */
     private static void drawHorizontalLine(int n) {
         //iterate n times to print n *
         for(int i = 0; i < n; i++){
@@ -95,6 +102,10 @@ public class Main {
         System.out.println();
     }
 
+    /**
+     * Given a number n, print n lines, each with one asterisks
+     * @param n
+     */
     private static void drawVerticalLine(int n) {
         //iterate n times to print n *
         for(int i = 0; i < n; i++){
@@ -102,6 +113,10 @@ public class Main {
         }
     }
 
+    /**
+     * Given a number n, print a centered triangle.
+     * @param n
+     */
     private static void isoscelesTriangle(int n) {
         for (int i = 1; i <= n; i++){
             for (int j = 1; j <= n-i; j++){
@@ -115,6 +130,10 @@ public class Main {
         }
     }
 
+    /**
+     * Given a number n, print a centered diamond.
+     * @param n
+     */
     private static void diamond(int n) {
         isoscelesTriangle(n);
         for (int i = n-1; i >= 1; i--){
@@ -129,6 +148,10 @@ public class Main {
         }
     }
 
+    /**
+     * Given a number n, print a reverse centered diamond.
+     * @param n
+     */
     private static void reverseIsoscelesTriangle(int n) {
         for (int i = n; i >= 1; i--){
             for (int j = n-i; j >= 1; j--){
@@ -142,12 +165,22 @@ public class Main {
         }
     }
 
+    /**
+     * Given a number n, print a centered diamond with your name in place of the middle line.
+     * @param n
+     * @param name
+     */
     private static void diamondWithName(int n, String name) {
         isoscelesTriangle(n-1);
         System.out.println(name);
         reverseIsoscelesTriangle(n-1);
     }
 
+    /**
+     * Instead of numbers divisible by three print "Fizz".
+     * Instead of numbers divisible by five print "Buzz".
+     * Instead of numbers divisible by three and five print "FizzBuzz".
+     */
     private static void fizzBuzz(){
         for (int i = 1; i <= 100; i++){
             if(i%3 == 0 && i%5 == 0){
@@ -162,6 +195,11 @@ public class Main {
         }
     }
 
+    /**
+     * Given an integer N will return a list of integers such that the numbers are the factors of N and are arranged in increasing numerical order
+     * @param n
+     * @param factorsList
+     */
     private static void generate(int n, List<Integer> factorsList){
         int factor = 2;
 
@@ -174,6 +212,15 @@ public class Main {
             }else{
                 factor++;
             }
+        }
+    }
+
+    private static void primeFactorsExercise() {
+        List<Integer> primeFactors = new ArrayList<>();
+        generate(askNumber(), primeFactors);
+
+        for(Integer fact : primeFactors){
+            System.out.println(fact);
         }
     }
 }
