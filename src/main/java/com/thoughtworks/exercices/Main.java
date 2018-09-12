006e82b1d393fc85b5b83ee4a0c3bcdee3ccae98
noemi <noemi.muste@thoughtworks.com>
2018-09-12 16:53:38 +0200

    Prime Factors Exercise

--- src/main/java/com/thoughtworks/exercices/Main.java
+++ src/main/java/com/thoughtworks/exercices/Main.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.exercices;
 
+import org.omg.CORBA.INTERNAL;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("TRIANGLE EXERCISES");
+       System.out.println("TRIANGLE EXERCISES");
         //Triangle Exercises
         System.out.println("Easiest exercise ever");
         triangleExercises();
@@ -36,12 +40,41 @@ public class Main {
         System.out.println("Diamond");
         diamond(askNumber());
         System.out.println();
+
+        //Diamond with name
+        System.out.println("Diamond with Name");
+        diamondWithName(askNumber(), askName());
+        System.out.println();
+
+        //FizzBuzz Exercise
+        System.out.println("FizzBuzz Exercise");
+        fizzBuzz();
+        System.out.println();
+
+        //Prime Factors Exercise
+        System.out.println("Prime Factors Exercise");
+        primeFactorsExercise();
+    }
+
+    private static void primeFactorsExercise() {
+        List<Integer> primeFactors = new ArrayList<>();
+        generate(askNumber(), primeFactors);
+
+        for(Integer fact : primeFactors){
+            System.out.println(fact);
+        }
     }
 
     private static int askNumber(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduce a number: ");
         return scanner.nextInt();
+    }
+
+    private static String askName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce a name: ");
+        return scanner.next();
     }
 
     private static void drawARightTriangle(int n) {
@@ -95,6 +128,54 @@ public class Main {
                 System.out.print("*");
             }
             System.out.println();
+        }
+    }
+
+    private static void reverseIsoscelesTriangle(int n) {
+        for (int i = n; i >= 1; i--){
+            for (int j = n-i; j >= 1; j--){
+                System.out.print(" ");
+            }
+
+            for (int j = i*2-1; j >= 1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void diamondWithName(int n, String name) {
+        isoscelesTriangle(n-1);
+        System.out.println(name);
+        reverseIsoscelesTriangle(n-1);
+    }
+
+    private static void fizzBuzz(){
+        for (int i = 1; i <= 100; i++){
+            if(i%3 == 0 && i%5 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i%3 == 0){
+                System.out.println("Fizz");
+            }else if(i%5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static void generate(int n, List<Integer> factorsList){
+        int factor = 2;
+
+        while(n != 1){
+            if(n % factor == 0){
+                n = n / factor;
+                generate(n, factorsList);
+                factorsList.add(0,factor);
+                break;
+            }else{
+                factor++;
+            }
         }
     }
 }
